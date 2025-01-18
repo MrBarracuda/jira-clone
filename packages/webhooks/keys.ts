c722aabb3a69d7eadd10c4688d488f1f3dadf918
mrbarracuda <1dima9999@gmail.com>
2025-01-18 21:07:25 +0100

    refactor: comment validation for unused packages

--- packages/webhooks/keys.ts
+++ packages/webhooks/keys.ts
@@ -4,14 +4,14 @@ import { z } from 'zod';
 export const keys = () =>
   createEnv({
     server: {
-      SVIX_TOKEN: z
-        .union([
-          z.string().min(1).startsWith('sk_'),
-          z.string().min(1).startsWith('testsk_'),
-        ])
-        .optional(),
+      // SVIX_TOKEN: z
+      //   .union([
+      //     z.string().min(1).startsWith('sk_'),
+      //     z.string().min(1).startsWith('testsk_'),
+      //   ])
+      //   .optional(),
     },
     runtimeEnv: {
-      SVIX_TOKEN: process.env.SVIX_TOKEN,
+      // SVIX_TOKEN: process.env.SVIX_TOKEN,
     },
   });
